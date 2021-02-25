1d2284eff6c175a5a0fb2375e9a9f8089007d576
programmeraadi <77089597+programmeraadi@users.noreply.github.com>
2021-02-25 16:41:13 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const { ApolloServer, gql } = require("apollo-server-express");
+const typeDefs = gql`
+  type Query {
+    name: String
+    country: String
+    ppsize: String
+    face: String
+    crush: String
+  }
+`;
+const resolvers = {
+  Query: {
+    name: () => "Samuel Miller",
+    country: () => "Venezuela",
+    ppsize: () => "smol",
+    face: () => "sexy face",
+    crush: () => "Lana Rhoades"
+  }
+};
+const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+server.applyMiddleware({ app });
+app.use((req, res) => {
+  res.status(200);
+  res.send("Hello!");
+  res.end();
+});
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+);
